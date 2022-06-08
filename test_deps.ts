d714a202f2d6c00c78c9d2c70599b215c4e28e0e
idanran <idanran@outlook.com>
2022-06-08 19:56:02 +0800

    test

--- test_deps.ts
+++
@@ -1,3 +0,0 @@
-export { describe, it, beforeEach, afterEach } from 'https://deno.land/std@0.142.0/testing/bdd.ts'
-export { equal } from 'https://deno.land/std@0.142.0/testing/asserts.ts'
-export { FakeTime } from 'https://deno.land/std@0.142.0/testing/time.ts'
