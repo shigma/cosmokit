bcb75c6a71492852614d648e5b2ec9dc1ca1a5bd
Shigma <shigma10826@gmail.com>
2022-09-20 23:49:38 +0800

    feat: add is()

--- src/misc.ts
+++ src/misc.ts
@@ -20,12 +20,17 @@ export function valueMap<T, U>(object: Dict<T>, transform: (value: T, key: strin
   return Object.fromEntries(Object.entries(object).map(([key, value]) => [key, transform(value, key)]))
 }
 
+export function is<K extends keyof typeof globalThis>(type: K, value: any): value is InstanceType<typeof globalThis[K]> {
+  return type in globalThis && value instanceof (globalThis[type] as any)
+    || Object.prototype.toString.call(value).slice(8, -1) === type
+}
+
 export function clone<T>(source: T): T
 export function clone(source: any) {
   if (!source || typeof source !== 'object') return source
   if (Array.isArray(source)) return source.map(clone)
-  if (source instanceof Date) return new Date(source.valueOf())
-  if (source instanceof RegExp) return new RegExp(source.source, source.flags)
+  if (is('Date', source)) return new Date(source.valueOf())
+  if (is('RegExp', source)) return new RegExp(source.source, source.flags)
   return valueMap(source, clone)
 }
 
